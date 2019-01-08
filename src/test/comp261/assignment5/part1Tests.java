21cf94ba31342bd410ce33c06fba4c3e0cf59787
N0nny <fletchrhia@ecs.vuw.ac.nz>
2019-01-08 16:33:45 +0000

    Initialising project with files

---
+++ src/test/comp261/assignment5/part1Tests.java
@@ -0,0 +1,14 @@
+package test.comp261.assignment5;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class part1Tests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
